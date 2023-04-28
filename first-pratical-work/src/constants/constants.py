5bedda02e174c6ee38b9d95311d7e412a38e61e8
lucianobajr <luciano.alcantara@ufv.br>
2023-04-28 08:39:37 -0300

    add(+): some adjuts

--- first-pratical-work/src/constants/constants.py
+++ first-pratical-work/src/constants/constants.py
@@ -8,13 +8,13 @@ class Constants:
         alternative = self.file_name.split('./data/')[1].split('.')[0]
 
         # 1 ou 2
-        evaluate_number_in_str =  '1' if alternative == 'a' or 'b' else '2'
+        evaluate_number_in_str =  '1' if alternative == 'a' or alternative == 'b' else '2'
 
         # Boxplot HC - (1-a)
-        title_boxplot = 'Boxplot ' + self.algorithm + ' - (' + evaluate_number_in_str + '-' + alternative
+        title_boxplot = 'Boxplot ' + self.algorithm + ' - (' + evaluate_number_in_str + '-' + alternative + ')'
         label_boxplot = 'Value of objective function'
         
         # HC_1_a
-        final_file_name = self.algorithm + '_' + evaluate_number_in_str + '_' + alternative
+        final_file_name = evaluate_number_in_str + '/' +self.algorithm + '-' + evaluate_number_in_str + '-' + alternative
 
         return title_boxplot,label_boxplot, final_file_name
